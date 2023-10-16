5153880907e00474df211f86bdc71dd1aef44834
ShawnZhuPro <shawnz05827@gmail.com>
2023-10-16 08:08:45 -0700

    Create draft of teleop swerve cmd, add comments on how to find swerve constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,54 +16,75 @@ public final class Constants {
 
   // Change these constants to ones measured for your robot
   public static final class ModuleConstants {
+    // Diameter of the swerve module's wheels in meters
     public static final double kWheelDiameterMeters = 0;
+
+    // Gear ratios for drive and turning motors
     public static final double kDriveMotorGearRatio = 0;
     public static final double kTurningMotorGearRatio = 0;
+
+    // Calculate the conversion factor from drive motor encoder rotations to meters
     public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
+
+    // Calculate the conversion factor from turning motor encoder rotations to radians
     public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
+
+    // Calculate the conversion factor from drive motor encoder RPM to meters per second
     public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+
+    // Calculate the conversion factor from turning motor encoder RPM to radians per second
     public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
+
+    // Proportional tuning constant for turning control
     public static final double kPTurning = 0;
   }
 
   // Change these constants to ones measured for your robot
   public static final class DriveConstants {
-    public static final double kPhysicalMaxSpeedMetersPerSecond = 0.0;
-
+    // Port numbers for drive motors on different swerve modules
     public static final int kFrontLeftDriveMotorPort = 0;
     public static final int kBackLeftDriveMotorPort = 0;
     public static final int kFrontRightDriveMotorPort = 0;
     public static final int kBackRightDriveMotorPort = 0;
 
+    // Port numbers for turning motors on different swerve modules
     public static final int kFrontLeftTurningMotorPort = 0;
     public static final int kBackLeftTurningMotorPort = 0;
     public static final int kFrontRightTurningMotorPort = 0;
     public static final int kBackRightTurningMotorPort = 0;
 
+    // Flags to reverse the turning encoder direction on swerve modules
     public static final boolean kFrontLeftTurningEncoderReversed = true;
     public static final boolean kBackLeftTurningEncoderReversed = true;
     public static final boolean kFrontRightTurningEncoderReversed = true;
     public static final boolean kBackRightTurningEncoderReversed = true;
 
+    // Flags to reverse the drive encoder direction on swerve modules
     public static final boolean kFrontLeftDriveEncoderReversed = true;
     public static final boolean kBackLeftDriveEncoderReversed = true;
     public static final boolean kFrontRightDriveEncoderReversed = true;
     public static final boolean kBackRightDriveEncoderReversed = true;
 
+    // Port numbers for absolute drive encoders on swerve modules
     public static final int kFrontLeftDriveAbsoluteEncoderPort = 0;
     public static final int kBackLeftDriveAbsoluteEncoderPort = 0;
     public static final int kFrontRightDriveAbsoluteEncoderPort = 0;
     public static final int kBackRightDriveAbsoluteEncoderPort = 0;
 
+    // Flags to reverse the absolute drive encoder direction on swerve modules
     public static final boolean kFrontLeftDriveAbsoluteEncoderReversed = true;
     public static final boolean kBackLeftDriveAbsoluteEncoderReversed = true;
     public static final boolean kFrontRightDriveAbsoluteEncoderReversed = true;
     public static final boolean kBackRightDriveAbsoluteEncoderReversed = true;
 
+    // Offset angles in radians for absolute drive encoders on swerve modules
     public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = 0;
     public static final double kBackLeftDriveAbsoluteEncoderOffsetRad = 0;
     public static final double kFrontRightDriveAbsoluteEncoderOffsetRad = 0;
     public static final double kBackRightDriveAbsoluteEncoderOffsetRad = 0;
+
+    // Maximum physical speed in meters per second
+    public static final double kPhysicalMaxSpeedMetersPerSecond = 0;
   }
 
 }
---
+++ src/main/java/frc/robot/commands/SwerveJoystickCmd.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class SwerveJoystickCmd extends CommandBase {
+  
+  private final SwerveSubsystem swerveSubsystem;
+  private final Supplier<Double> xSpdFunction, ySpdFunction, turningSpdFunction;  // X, Y, and turning speed values
+  private final Supplier<Boolean> fieldOrientedFunction;  // Determines if the user wants the joystick cmd to be field-oriented
+
+
+  // Constructor that "constructs" a SwerveJoystickCmd command given values in the parameters ()
+  public SwerveJoystickCmd(SwerveSubsystem swerveSubsystem,
+  Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction, Supplier<Double> turningSpdFunction,
+  Supplier<Boolean> fieldOrientedFunction) {
+    this.swerveSubsystem = swerveSubsystem;
+    this.xSpdFunction = xSpdFunction;
+    this.ySpdFunction = ySpdFunction;
+    this.turningSpdFunction = turningSpdFunction;
+    this.fieldOrientedFunction = fieldOrientedFunction;  
+    addRequirements(swerveSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
